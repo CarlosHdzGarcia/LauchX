afcc76dbb90ee176af07c9fcfea2e313cc19eef0
CarlosHdzGarcia <99059536+CarlosHdzGarcia@users.noreply.github.com>
2022-02-20 23:51:15 -0600

    Add files via upload

---
+++ Modulo1/modulo1/modulo1.py
@@ -0,0 +1,25 @@
+# modulo1.py
+sum = 1+2 
+
+print(sum)
+
+producto= sum * 2 
+print(producto)
+hola='hola'
+type(producto)
+print(type(hola))
+
+from datetime import date
+date.today()
+
+print(date.today())
+
+
+print('Today is :'+ str(date.today()))
+
+print('calculadora')
+num1= input('primer Numero')
+num2 = input('Segundo numero')
+print(num1 +   num2)
+
+print(int(num1)+int(num2))
